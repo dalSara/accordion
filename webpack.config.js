64b77f6b47a0f6c76d36d4aa1bad370e7ddf3ff1
dalSara <saradalfalk@gmail.com>
2020-01-19 21:13:34 +0100

    css-loader and config

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,30 @@
-const path = require('path');
+const path = require("path");
 
 module.exports = {
-    entry: './src/index.jsx',
-    output: {
-        filename: 'bundle.js',
-        path: path.resolve(__dirname, 'public')
-    },
-    module: {
-        rules: [
-            {
-                test: /\.jsx$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader"
-                }
-            }
-        ]
-    },
-    resolve: {
-        extensions: ['.js', '.jsx']
-    },
-    devServer: {
-        contentBase: './public'
-    }
+  entry: "./src/index.jsx",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "public")
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader"
+        }
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"]
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".js", ".jsx"]
+  },
+  devServer: {
+    contentBase: "./public"
+  }
 };
